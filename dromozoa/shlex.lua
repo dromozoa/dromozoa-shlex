7935b0a58037c068b81004189f847af33eb99053
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-02-05 09:59:44 +0900

    コードを整理。

--- dromozoa/shlex.lua
+++ dromozoa/shlex.lua
@@ -16,92 +16,86 @@
 -- along with dromozoa-shlex.  If not, see <http://www.gnu.org/licenses/>.
 
 local utf8 = require "dromozoa.utf8"
-local unpack = table.unpack or unpack
 
--- quotes
--- escapedquotes
--- wordchars
-
--- mode
---   initial      nil
---   single quote 0x27
---   double quote 0x22
---   white space  0x20,0x09,0x0d,0x0a
--- escape         0x5C
-
-local SQ = 0x27
-local DQ = 0x22
-local SP = 0x20
-local HT = 0x09
-local LF = 0x0A
-local CR = 0x0D
-local BS = 0x5C
+local SQ = 0x27 -- U+0027 APORSTROPHE
+local DQ = 0x22 -- U+0022 QUOTATION MARK
+local SP = 0x20 -- U+0020 SPACE
+local HT = 0x09 -- U+0009 CHARACTER TABULATION
+local LF = 0x0A -- U+000A LINE FEED (LF)
+local CR = 0x0D -- U+000D CARRIAGE RETURN (CR)
+local BS = 0x5C -- U+005C REVERSE SOLIDUS
 
 local function split(s)
-  local result = {}
-
   local token
   local state
-  local escape
+  local escape = false
+  local result = {}
   for p, c in utf8.codes(s) do
+    local s = utf8.char(c)
     if state == SQ then
       if c == SQ then
         state = nil
       else
-        token[#token + 1] = c
+        token[#token + 1] = s
       end
     elseif state == DQ then
       if escape then
-        if c ~= DQ and c ~= BS then
-          token[#token + 1] = BS
+        if c == DQ or c == BS then
+          token[#token + 1] = s
+        else
+          token[#token + 1] = "\\"
+          token[#token + 1] = s
         end
-        token[#token + 1] = c
-        escape = nil
+        escape = false
       else
         if c == DQ then
           state = nil
         elseif c == BS then
           escape = true
         else
-          token[#token + 1] = c
+          token[#token + 1] = s
         end
       end
     else
       if escape then
-        token[#token + 1] = c
-        escape = nil
+        token[#token + 1] = s
+        escape = false
       else
-        if c == SQ then
-          state = SQ
-          if token == nil then token = {} end
-        elseif c == DQ then
-          state = DQ
-          if token == nil then token = {} end
-        elseif c == BS then
-          escape = true
-          if token == nil then token = {} end
-        elseif c == SP or c == HT or c == LF or c == CR then
+        if c == SP or c == HT or c == LF or c == CR then
           if token ~= nil then
-            result[#result + 1] = utf8.char(unpack(token))
+            result[#result + 1] = table.concat(token)
             token = nil
           end
         else
           if token == nil then token = {} end
-          token[#token + 1] = c
+          if c == SQ then
+            state = SQ
+          elseif c == DQ then
+            state = DQ
+          elseif c == BS then
+            escape = true
+          else
+            token[#token + 1] = s
+          end
         end
       end
     end
   end
 
-  assert(state == nil)
-  assert(not escape)
+  if state ~= nil then
+    error "no closing quotation"
+  end
+  if escape then
+    error "no escaped character"
+  end
+
   if token ~= nil then
-    result[#result + 1] = utf8.char(unpack(token))
-    token = nil
+    result[#result + 1] = table.concat(token)
   end
   return result
 end
 
 return {
   split = split;
+  version = function () return "1.0" end;
 }
